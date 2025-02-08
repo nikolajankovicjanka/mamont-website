0b21eabc7312aa7b7a18d01930e2365fc868f81a
Nikola <nikolajankovicjanka@gmail.com>
2025-02-08 23:20:30 +0100

    feat: Dodat swiper slider i funcionalnost na navbaru

--- src/assets/images/index.js
+++ src/assets/images/index.js
@@ -1,13 +1,19 @@
 
 const images = {
-    mamont: require('@/assets/images/images/mamont.jpg'),
-    mamontLogo: require('@/assets/images/images/mamont-logo.png'),
-    whoWeAre: require('@/assets/images/images/whoweare.jpg'),
-    image1:  require('@/assets/images/images/image1.jpg'),
-    image2:  require('@/assets/images/images/image2.jpg'),
-    image3:  require('@/assets/images/images/image3.jpg'),
-    image4:  require('@/assets/images/images/image4.jpg'),
-    image5:  require('@/assets/images/images/image5.jpg'),
+    mamont: require('@/assets/images/test_images/mamont.jpg'),
+    mamontLogo: require('@/assets/images/test_images/mamont-logo.png'),
+    whoWeAre: require('@/assets/images/test_images/whoweare.jpg'),
+    image1:  require('@/assets/images/test_images/image1.jpg'),
+    image2:  require('@/assets/images/test_images/image2.jpg'),
+    image3:  require('@/assets/images/test_images/image3.jpg'),
+    image4:  require('@/assets/images/test_images/image4.jpg'),
+    image5:  require('@/assets/images/test_images/image5.jpg'),
+    mamontProject: require('@/assets/images/mamont_images/mamont.jpg'),
+    mamontProject3: require('@/assets/images/mamont_images/mamont3.jpg'),
+    mamontProject4: require('@/assets/images/mamont_images/mamont4.jpg'),
+    mamontProject5: require('@/assets/images/mamont_images/mamont5.png'),
+    mamontProject6: require('@/assets/images/mamont_images/mamont6.jpg'),
+    mamontProject7: require('@/assets/images/mamont_images/mamont7.jpg'),
 }
 
 export default images
--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -1,58 +1,49 @@
 <template>
-    <header>
-        <div class="container">
-            <div>
-                <b-navbar toggleable="lg" type="dark" variant="transparent">
-                    <b-navbar-brand href="#">
-                        <img :src="mamontLogo" style="max-width: 200px" alt="Logo" class="navbar-logo" />
-                    </b-navbar-brand>
+    <b-navbar toggleable="lg" type="dark" variant="transparent" class="fixed-navbar" ref="navbar">
+        <b-navbar-brand href="#">
+            <img :src="mamontLogo" style="max-width: 200px" alt="Logo" class="navbar-logo" />
+        </b-navbar-brand>
 
-                    <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+        <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
 
-                    <b-collapse class="justify-content-end" id="nav-collapse" is-nav>
-                        <b-navbar-nav class="font-header">
-                            <b-nav-item href="#">O nama</b-nav-item>
-                            <b-nav-item href="#">Usluge</b-nav-item>
-                            <b-nav-item href="#">Projekti</b-nav-item>
-                            <b-nav-item href="#">Kontakti</b-nav-item>
-                        </b-navbar-nav>
+        <b-collapse class="justify-content-end" id="nav-collapse" is-nav>
+            <b-navbar-nav class="font-header">
+                <b-nav-item href="#o-nama">O nama</b-nav-item>
+                <b-nav-item href="#usluge">Usluge</b-nav-item>
+                <b-nav-item href="#projekti">Projekti</b-nav-item>
+                <b-nav-item href="#kontakti">Kontakti</b-nav-item>
+            </b-navbar-nav>
 
-                        <!-- Right aligned nav items -->
-                        <b-navbar-nav class="ml-auto">
-
-                            <b-nav-item-dropdown text="SR" right>
-                                <b-dropdown-item href="#">EN</b-dropdown-item>
-                                <b-dropdown-item href="#">DE</b-dropdown-item>
-                            </b-nav-item-dropdown>
-                        </b-navbar-nav>
-                    </b-collapse>
-                </b-navbar>
-            </div>
-        </div>
-        <div class="container">
-            <div class="carousel-container">
+            <!-- Right aligned nav items -->
+            <b-navbar-nav class="ml-auto">
+                <b-nav-item-dropdown text="SR" right>
+                    <b-dropdown-item href="#">EN</b-dropdown-item>
+                    <b-dropdown-item href="#">DE</b-dropdown-item>
+                </b-nav-item-dropdown>
+            </b-navbar-nav>
+        </b-collapse>
+    </b-navbar>
+        <div class="container mb-4">
                 <swiper
-                        :slides-per-view="3"
-                        :space-between="20"
-                        :loop="true"
+                        :modules="modules"
+                        :slides-per-view="1"
+                        :space-between="50"
                         navigation
                         pagination
+                        :autoplay="{ delay: 3000, disableOnInteraction: false }"
+                        :speed="1000"
+                        @swiper="onSwiper"
+                        @slideChange="onSlideChange"
                 >
-                    <swiper-slide v-for="(item, index) in items" :key="index">
-                        <div class="carousel-item">
-                            <img :src="item.image" :alt="item.title" />
-                            <h3>{{ item.title }}</h3>
-                        </div>
+                    <swiper-slide v-for="(image, index) in images" :key="index">
+                        <img :src="image" :alt="'Slide ' + (index + 1)" class="slide-image" />
                     </swiper-slide>
                 </swiper>
-            </div>
         </div>
-    </header>
-
-    <div class="container">
+    <div id="o-nama" class="container mt-4">
         <div class="project-title title">
-            <h1 class="primary-color">O nama</h1>
-            <h5 class="primary-color">Naša firma je na tržištu centralne evrope zastupljena i bavi se velikim brojem usluga iz oblasti montaže, ugradnje, instalacija i mnogih drugih usluga Elektro energije.</h5>
+            <h1 class="primary-color custom-border">O nama</h1>
+            <h5 class="primary-color mb-4">Naša firma je na tržištu centralne evrope zastupljena i bavi se velikim brojem usluga iz oblasti montaže, ugradnje, instalacija i mnogih drugih usluga Elektro energije.</h5>
         </div>
         <section class="about-section">
             <div class="container">
@@ -87,10 +78,10 @@
     </div>
 
     <section class="news-section news-section-home">
-        <div class="container">
+        <div id="usluge" class="container mt-4">
             <div class="project-title title">
-                <h1 class="primary-color">Naše usluge</h1>
-                <h5 class="primary-color">Naša firma je na tržištu centralne evrope zastupljena i bavi se velikim brojem usluga iz oblasti montaže, ugradnje, instalacija i mnogih drugih usluga Elektro energije.</h5>
+                <h1 class="primary-color custom-border">Naše usluge</h1>
+                <h5 class="primary-color mb-4">Naša firma je na tržištu centralne evrope zastupljena i bavi se velikim brojem usluga iz oblasti montaže, ugradnje, instalacija i mnogih drugih usluga Elektro energije.</h5>
             </div>
             <div class="news-box">
                 <div class="row">
@@ -162,166 +153,26 @@
         </div>
     </section>
 
-    <div class="container">
+    <div id="projekti" class="container mb-4">
         <section class="projects-section">
+            <div class="project-title title">
+                <h1 class="primary-color custom-border">Naši projekti</h1>
+                <h5 class="primary-color mb-4">Naša firma je na tržištu centralne evrope zastupljena i bavi se velikim brojem usluga iz oblasti montaže, ugradnje, instalacija i mnogih drugih usluga Elektro energije.</h5>
+            </div>
             <div class="container">
-                <div class="project-title title">
-                    <span class="sm-title">Great & awesome works</span>
-                    <h2 class="box-header">OUR Projects</h2>
-                    <p>Duis aute irure dolor in reprehenderit in voluptate velit esse
-                        cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-                        proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-                </div> </div>
-            <ul class="filter">
-                <li><a class="active" href="#" data-filter="*">Show All</a></li>
-                <li><a href="#" data-filter=".clean">Clean</a></li>
-                <li><a href="#" data-filter=".engine">Engine Repair</a></li>
-                <li><a href="#" data-filter=".oil">Oil Changes</a></li>
-                <li><a href="#" data-filter=".mufflers">Mufflers</a></li>
-            </ul>
-            <div class="project-box iso-call d-flex justify-content-around">
-                <div class="project-post clean engine oil mufflers">
-                    <img :src="image3" alt="">
-                    <div class="hover-box">
-                        <ul class="list-unstyled">
-                            <li><a href="projects-details.html"><i class="icon icon-Linked"></i></a></li>
-                            <li><a href="upload/projects/1.jpg" data-source="projects-details.html" title="KITCHKEN RENOVATION" data-desc="Wood Work of Rack" class="popup">
-                                <i class="icon icon-Search"></i></a></li>
-                        </ul>
-                        <div class="bx">
-                            <h2><a href="single-project.html">electrician Works</a></h2>
-                            <span>Duis aute irure dolor in reprehenderit in voluptate velit esse</span>
-                        </div>
-                    </div>
-                </div>
-                <div class="project-post clean oil mufflers ">
-                    <img :src="image3" alt="">
-                    <div class="hover-box">
-                        <ul class="list-unstyled">
-                            <li><a href="projects-details.html"><i class="icon icon-Linked"></i></a></li>
-                            <li><a href="upload/projects/2.jpg" data-source="projects-details.html" title="KITCHKEN RENOVATION" data-desc="Wood Work of Rack" class="popup">
-                                <i class="icon icon-Search"></i></a></li>
-                        </ul>
-                        <div class="bx">
-                            <h2><a href="single-project.html">electrician Works</a></h2>
-                            <span>Duis aute irure dolor in reprehenderit in voluptate velit esse</span>
-                        </div>
-                    </div>
-                </div>
-                <div class="project-post engine oil mufflers">
-                    <img :src="image3" alt="">
-                    <div class="hover-box">
-                        <ul class="list-unstyled">
-                            <li><a href="projects-details.html"><i class="icon icon-Linked"></i></a></li>
-                            <li><a href="upload/projects/3.jpg" data-source="projects-details.html" title="KITCHKEN RENOVATION" data-desc="Wood Work of Rack" class="popup">
-                                <i class="icon icon-Search"></i></a></li>
-                        </ul>
-
-                        <div class="bx">
-                            <h2><a href="single-project.html">electrician Works</a></h2>
-                            <span>Duis aute irure dolor in reprehenderit in voluptate velit esse</span>
-
-                        </div>
-                    </div>
-                </div>
-                <div class="project-post clean engine oil">
-                    <img :src="image3" alt="">
-                    <div class="hover-box">
-                        <ul class="list-unstyled">
-                            <li><a href="projects-details.html"><i class="icon icon-Linked"></i></a></li>
-                            <li><a href="upload/projects/4.jpg" data-source="projects-details.html" title="KITCHKEN RENOVATION" data-desc="Wood Work of Rack" class="popup">
-                                <i class="icon icon-Search"></i></a></li>
-                        </ul>
-                        <div class="bx">
-                            <h2><a href="single-project.html">electrician Works</a></h2>
-                            <span>Duis aute irure dolor in reprehenderit in voluptate velit esse</span>
-                        </div>
-                    </div>
-                </div>
-                <div class="project-post clean engine mufflers">
-                    <img :src="image3" alt="">
-                    <div class="hover-box">
-                        <ul class="list-unstyled">
-                            <li><a href="projects-details.html"><i class="icon icon-Linked"></i></a></li>
-                            <li><a href="upload/projects/5.jpg" data-source="projects-details.html" title="KITCHKEN RENOVATION" data-desc="Wood Work of Rack" class="popup">
-                                <i class="icon icon-Search"></i></a></li>
-                        </ul>
-                        <div class="bx">
-                            <h2><a href="single-project.html">electrician Works</a></h2>
-                            <span>Duis aute irure dolor in reprehenderit in voluptate velit esse</span>
-                        </div>
-                    </div>
-                </div>
-                <div class="project-post clean mufflers">
-                    <img :src="image3" alt="">
-                    <div class="hover-box">
-                        <ul class="list-unstyled">
-                            <li><a href="projects-details.html"><i class="icon icon-Linked"></i></a></li>
-                            <li><a href="upload/projects/6.jpg" data-source="projects-details.html" title="KITCHKEN RENOVATION" data-desc="Wood Work of Rack" class="popup">
-                                <i class="icon icon-Search"></i></a></li>
-                        </ul>
-                        <div class="bx">
-                            <h2><a href="single-project.html">electrician Works</a></h2>
-                            <span>Duis aute irure dolor in reprehenderit in voluptate velit esse</span>
-                        </div>
-                    </div>
-                </div>
-                <div class="project-post engine oil">
-                    <img :src="image3" alt="">
-                    <div class="hover-box">
-                        <ul class="list-unstyled">
-                            <li><a href="projects-details.html"><i class="icon icon-Linked"></i></a></li>
-                            <li><a href="upload/projects/7.jpg" data-source="projects-details.html" title="KITCHKEN RENOVATION" data-desc="Wood Work of Rack" class="popup">
-                                <i class="icon icon-Search"></i></a></li>
-                        </ul>
-                        <div class="bx">
-                            <h2><a href="single-project.html">electrician Works</a></h2>
-                            <span>Duis aute irure dolor in reprehenderit in voluptate velit esse</span>
-                        </div>
-                    </div>
-                </div>
-                <div class="project-post oil mufflers">
-                    <img :src="image3" alt="">
-                    <div class="hover-box">
-                        <ul class="list-unstyled">
-                            <li><a href="projects-details.html"><i class="icon icon-Linked"></i></a></li>
-                            <li><a href="upload/projects/8.jpg" data-source="projects-details.html" title="KITCHKEN RENOVATION" data-desc="Wood Work of Rack" class="popup">
-                                <i class="icon icon-Search"></i></a></li>
-                        </ul>
-                        <div class="bx">
-                            <h2><a href="single-project.html">electrician Works</a></h2>
-                            <span>Duis aute irure dolor in reprehenderit in voluptate velit esse</span>
-                        </div>
-                    </div>
-                </div>
-                <div class="project-post clean engine ">
-                    <img :src="image3" alt="">
-                    <div class="hover-box">
-                        <ul class="list-unstyled">
-                            <li><a href="projects-details.html"><i class="icon icon-Linked"></i></a></li>
-                            <li><a href="upload/projects/9.jpg" data-source="projects-details.html" title="KITCHKEN RENOVATION" data-desc="Wood Work of Rack" class="popup">
-                                <i class="icon icon-Search"></i></a></li>
-                        </ul>
-                        <div class="bx">
-                            <h2><a href="single-project.html">electrician Works</a></h2>
-                            <span>Duis aute irure dolor in reprehenderit in voluptate velit esse</span>
-                        </div>
-                    </div>
-                </div>
-                <div class="project-post clean engine oil mufflers">
-                    <img :src="image3" alt="">
-                    <div class="hover-box">
-                        <ul class="list-unstyled">
-                            <li><a href="projects-details.html"><i class="icon icon-Linked"></i></a></li>
-                            <li><a href="upload/projects/10.jpg" data-source="projects-details.html" title="KITCHKEN RENOVATION" data-desc="Wood Work of Rack" class="popup">
-                                <i class="icon icon-Search"></i></a></li>
-                        </ul>
-                        <div class="bx">
-                            <h2><a href="single-project.html">electrician Works</a></h2>
-                            <span>Duis aute irure dolor in reprehenderit in voluptate velit esse</span>
-                        </div>
-                    </div>
-                </div>
+                <swiper
+                        :modules="modules"
+                        :slides-per-view="1"
+                        :space-between="50"
+                        navigation
+                        pagination
+                        @swiper="onSwiper"
+                        @slideChange="onSlideChange"
+                >
+                    <swiper-slide v-for="(image, index) in imagesGalery" :key="index">
+                        <img :src="image" :alt="'Slide ' + (index + 1)" class="slide-image" />
+                    </swiper-slide>
+                </swiper>
             </div>
         </section>
     </div>
@@ -330,7 +181,10 @@
 
 <script>
 import { Swiper, SwiperSlide } from 'swiper/vue';
+import { Navigation, Pagination, Autoplay } from 'swiper/modules';
+import 'swiper/swiper-bundle.css';
 import FooterHomePage from './common/FooterHomePage.vue'
+import { ref, onMounted, onBeforeUnmount } from 'vue';
 export default {
     name: 'HomePage',
     components: {
@@ -346,15 +200,76 @@ export default {
           imageHomepage: this.$img('image1'),
           mamontLogo: this.$img('mamontLogo'),
           image3: this.$img('image3'),
-          items: [
-              { title: 'Slide 1', image: this.$img('image1') },
-              { title: 'Slide 2', image: this.$img('image2') },
-              { title: 'Slide 3', image: this.$img('image3') },
-              { title: 'Slide 4', image: this.$img('image4') },
-              { title: 'Slide 5', image: this.$img('image5') },
-          ],
+          visible: false, // Kontrola vidljivosti lightbox-a
+          index: 0, // Indeks trenutno odabrane slike
       }
     },
+    setup() {
+        // Ref za navbar element
+        const navbar = ref(null);
+
+        // Funkcija za rukovanje skrolanjem
+        const handleScroll = () => {
+            if (window.scrollY > 50) {
+                navbar.value.classList.add('sticky');
+            } else {
+                navbar.value.classList.remove('sticky');
+            }
+        };
+
+        // Dodaj event listener prilikom montiranja komponente
+        onMounted(() => {
+            navbar.value = document.querySelector('.fixed-navbar'); // Postavi referencu na navbar
+            window.addEventListener('scroll', handleScroll);
+        });
+
+        // Ukloni event listener prilikom unmount-a komponente
+        onBeforeUnmount(() => {
+            window.removeEventListener('scroll', handleScroll);
+        });
+
+        const images = [
+            require('@/assets/images/test_images/image1.jpg'),
+            require('@/assets/images/test_images/image2.jpg'),
+            require('@/assets/images/test_images/image3.jpg'),
+        ];
+
+        const imagesGalery = [
+            require('@/assets/images/test_images/image1.jpg'),
+            require('@/assets/images/test_images/image1.jpg'),
+            require('@/assets/images/test_images/image1.jpg'),
+        ];
+
+        const onSwiper = (swiper) => {
+            console.log('Swiper instance:', swiper);
+        };
+
+        const onSlideChange = () => {
+            console.log('Slide changed');
+        };
+
+        return {
+            modules: [Navigation, Pagination, Autoplay],
+            images,
+            imagesGalery,
+            onSwiper,
+            onSlideChange,
+            handleScroll,
+            onMounted,
+            onBeforeUnmount,
+            ref,
+        };
+    },
+
+
+    methods: {
+        // Prikaz lightbox-a
+        showLightbox(index) {
+            this.index = index;
+            this.visible = true;
+        },
+    },
+
 }
 </script>
 
